4f6aeb9e0621ae5ca7477bb342898c82462f94c7
180608ar <73707937+180608ar@users.noreply.github.com>
2022-02-28 15:06:16 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,121 +4,92 @@ const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 
 var engine, world;
-var holder,ball,ground;
-var stand1,stand2;
-var ball;
-var slingShot;
-var polygon_img;
-function preload(){
-  polygon_img=loadImage("polygon.png");
-}
+var ground, sling, ball;
+var stand1, stand2;
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15;
+var py1, py2, py3, py4, py5;
+
 function setup() {
-  createCanvas(900,400);
+  createCanvas(1300, 400);
   engine = Engine.create();
   world = engine.world;
-  Engine.run(engine);
-  ground = new Ground();
-  stand1 = new Stand(390,300,250,10);
-  stand2 = new Stand(700,200,200,10);
- 
-  //level one
-  block1 = new Block(300,275,30,40);
-  console.log(block1);
-  block2 = new Block(330,275,30,40);
-  block3 = new Block(360,275,30,40);
-  block4 = new Block(390,275,30,40);
-  block5 = new Block(420,275,30,40);
-  block6 = new Block(450,275,30,40);
-  block7 = new Block(480,275,30,40);
-  //level two
-  block8 = new Block(330,235,30,40);
-  block9 = new Block(360,235,30,40);
-  block10 = new Block(390,235,30,40);
-  block11 = new Block(420,235,30,40);
-  block12 = new Block(450,235,30,40);
-  //level three
-  block13 = new Block(360,195,30,40);
-  block14 = new Block(390,195,30,40);
-  block15 = new Block(420,195,30,40);
-  //top
-  block16 = new Block(390,155,30,40);
-
-  //set 2 for second stand
-  //level one
-  blocks1 = new Block(640,175,30,40);
-  blocks2 = new Block(670,175,30,40);
-  blocks3 = new Block(700,175,30,40);
-  blocks4 = new Block(730,175,30,40);
-  blocks5 = new Block(760,175,30,40);
-  //level two
-  blocks6 = new Block(670,135,30,40);
-  blocks7 = new Block(700,135,30,40);
-  blocks8 = new Block(730,135,30,40);
-  //top
-  blocks9 = new Block(700,95,30,40);
-
-  //ball holder with slings
-  ball = Bodies.circle(50,200,20);
-  World.add(world,ball);
-
-  slingShot = new Slingshot(this.ball,{x:100,y:200});
-
+  ground = new Ground(750, 375, 1500, 50);
+  box1 = new Box(400, 230, 50, 50);
+  box2 = new Box(450, 230, 50, 50);
+  box3 = new Box(500, 230, 50, 50);
+  box4 = new Box(550, 230, 50, 50);
+  box5 = new Box(600, 230, 50, 50);
+  box6 = new Box(425, 180, 50, 50);
+  box7 = new Box(475, 180, 50, 50);
+  box8 = new Box(525, 180, 50, 50);
+  box9 = new Box(575, 180, 50, 50);
+  box10 = new Box(450, 130, 50, 50);
+  box11 = new Box(500, 130, 50, 50);
+  box12 = new Box(550, 130, 50, 50);
+  box13 = new Box(475, 80, 50, 50);
+  box14 = new Box(525, 80, 50, 50);
+  box15 = new Box(500, 30, 50, 50);
+  py1 = new Box(845, 150, 50, 50);
+  py2 = new Box(895, 150, 50, 50);
+  py3 = new Box(945, 150, 50, 50);
+  py4 = new Box(870, 100, 50, 50);
+  py5 = new Box(920, 100, 50, 50);
+  py6 = new Box(895, 50, 50, 50);
+  ball = new Polygon(100, 100, 30);
+  stand1 = new Ground(500, 270, 300, 20);
+  stand2 = new Ground(900, 200, 300, 20);
+  sling = new Sling(ball.body, { x: 200, y: 250 })
 }
-function draw() {
-  background(56,44,44); 
- 
-  //Engine.update(engine);
-  //text(mouseX + ',' + mouseY, 10, 15);
-  textSize(20);
-  fill("lightyellow");
-  text("Drag the Hexagonal Stone and Release it, to launch it towards the blocks",100,30);
 
+function draw() {
+  background(0);
+  Engine.update(engine);
+  rectMode(CENTER);
   ground.display();
+  box1.display("red", "pink");
+  box2.display("red", "pink");
+  box3.display("red", "pink");
+  box4.display("red", "pink");
+  box5.display("red", "pink");
+  box6.display("indigo", "pink");
+  box7.display("indigo", "pink");
+  box8.display("indigo", "pink");
+  box9.display("indigo", "pink");
+  box10.display("green", "pink");
+  box11.display("green", "pink");
+  box12.display("green", "pink");
+  box13.display("orange", "pink");
+  box14.display("orange", "pink");
+  box15.display("saffron", "pink");
+  py1.display(rgb(229, 116, 117), "cyan")
+  py2.display(rgb(229, 116, 117), "cyan")
+  py3.display(rgb(229, 116, 117), "cyan")
+  py4.display(rgb(110, 71, 102), "cyan")
+  py5.display(rgb(110, 71, 102), "cyan")
+  py6.display(rgb(251, 189, 76), "cyan")
+  sling.display("limegreen");
   stand1.display();
   stand2.display();
-  strokeWeight(2);
-  stroke(15);
-  fill("skyblue");
-  block1.display();
-  block2.display();
-  block3.display();
-  block4.display();
-  block5.display();
-  block6.display();
-  block7.display();
-  fill("pink");
-  block8.display();
-  block9.display();
-  block10.display();
-  block11.display();
-  block12.display();
-  fill("turquoise");
-  block13.display();
-  block14.display();
-  block15.display();
-  fill("grey");
-  block16.display();
-  fill("skyblue");
-  blocks1.display();
-  blocks2.display();
-  blocks3.display();
-  blocks4.display();
-  blocks5.display();
-  fill("turquoise");
-  blocks6.display();
-  blocks7.display();
-  blocks8.display();
-  fill("pink")
-  blocks9.display();
-  fill("gold");
-  imageMode(CENTER)
-  image(polygon_img ,ball.position.x,ball.position.y,40,40);
+  ball.display("darkblue", "orange");
+  textSize(35);
+  fill(0);
+  text("DRAG AND RELEASE THE BALL TO DESTROY THE BOXES", 150, 385)
+  fill("orange");
+  textSize(20);
+  text("PRESS SPACE FOR A SECOND CHANCE !!",20,20)
+}
 
-  slingShot.display();
+function mouseDragged() {
+  Matter.Body.setPosition(ball.body, { x: mouseX, y: mouseY })
 }
-function mouseDragged(){
-  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+
+
+function mouseReleased() {
+  sling.fly();
 }
-function mouseReleased(){
-  slingShot.fly();
+
+function keyPressed() {
+  if (keyCode === 32) {
+    sling.attach(ball.body);
+  }
 }
